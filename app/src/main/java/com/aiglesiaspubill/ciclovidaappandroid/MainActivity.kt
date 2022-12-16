b133b5036cbabf39dc3e582583b4dc923ba7e83d
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-16 15:33:20 +0100

    SHARED PREFERENCES/ Instance/ TOAST

--- app/src/main/java/com/aiglesiaspubill/ciclovidaappandroid/MainActivity.kt
+++ app/src/main/java/com/aiglesiaspubill/ciclovidaappandroid/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.aiglesiaspubill.ciclovidaappandroid
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Toast
+import com.aiglesiaspubill.ciclovidaappandroid.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    companion object {
+        const val TAG_NOM = "MyName"
+    }
     //PRIMER PASO SE CREA
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    val binding = ActivityMain
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        //SAVE INSTANCE
+        //Recogemos el String o null si no hubiera
+        val textAnterior = savedInstanceState?.getString(TAG_NOM)
+        //Si es null creamos nuevo valor
+        binding.tvView.text = textAnterior ?: Random().nextInt().toString()
+
+        //SHARED PREFERENCES
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        val preferencesText = sharedPreferences.getString(TAG_NOM, "")
+        println(preferencesText)
+
+        //TOAST
+        val time = System.currentTimeMillis()
+        Toast.makeText(this, "$time", Toast.LENGTH_LONG).show()
+
+
         Log.d(MainActivity::class.java.simpleName, "onCreate")
     }
 
@@ -44,6 +70,11 @@ class MainActivity : AppCompatActivity() {
 
     //CUANDO SE QUEDA ESPERANDO EL ACTIVITY A SI SE REINICIA O SE DESTRUYE
     override fun onStop() {
+
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        val editPreferences = sharedPreferences.edit()
+        editPreferences.putString(TAG_NOM, "Mi Nombre")
+        editPreferences.commit()
         super.onStop()
         Log.d(MainActivity::class.java.simpleName, "onStop")
     }
